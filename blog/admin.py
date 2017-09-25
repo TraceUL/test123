22ee878f8e8fdb9a94c9486bbdcff53d77b90c3e
car100 <car100@car100deMacBook-Pro.local>
2017-09-25 11:26:11 +0800

    test1

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+"""from django.contrib import admin
+
+# Register your models here.
+from .models import Article
+
+
+admin.site.register(Article)
+"""
+
+from django.contrib import admin
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','update_time',)
+
+admin.site.register(Article,ArticleAdmin)
---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField('title', max_length=256)
+    content = models.TextField('content')
+
+    pub_date = models.DateTimeField('publish-time', auto_now_add=True, editable = True)
+    update_time = models.DateTimeField('update-time',auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
